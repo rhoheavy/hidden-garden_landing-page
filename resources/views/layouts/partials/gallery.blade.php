54c38120ce1c07775e727e357e62d1a3ced9f2fa
Rena Salazar <renasalazart@outlook.com>
2022-11-22 10:23:19 -0700

    Galeria

--- resources/views/layouts/partials/gallery.blade.php
+++ resources/views/layouts/partials/gallery.blade.php
@@ -14,10 +14,11 @@
                         src="{{ asset('img/yard2.png') }}">
                     <div
                         class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                        <h2 class="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">THE SUBTITLE</h2>
-                        <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
-                        <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                            microdosing tousled waistcoat.</p>
+                        <h2 class="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">El Comienzo</h2>
+                        <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Heredero</h1>
+                        <p class="leading-relaxed">Débil y vulnerable se reúne con sus compañeros (Emma, Moon, este
+                            último quien ha sido herido de manera mortal) y crean un plan para poder derrotar al Capitán
+                            y recuperar el Reino. </p>
                     </div>
                 </div>
             </div>
@@ -27,10 +28,11 @@
                         src="{{ asset('img/jail4.png') }}">
                     <div
                         class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                        <h2 class="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">THE SUBTITLE</h2>
-                        <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
-                        <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                            microdosing tousled waistcoat.</p>
+                        <h2 class="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">La Prisión</h2>
+                        <h1 class="title-font text-lg font-medium text-gray-900 mb-3">En el Palacio</h1>
+                        <p class="leading-relaxed">Se trata del escenario en donde comienza el personaje principal, la
+                            cárcel de la ciudad. posee un pasillo que está contantemente vigilado y un área circular
+                            donde se encuentran más celdas y el comedor. </p>
                     </div>
                 </div>
             </div>
@@ -40,10 +42,12 @@
                         src="{{ asset('img/yard3.png') }}">
                     <div
                         class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                        <h2 class="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">THE SUBTITLE</h2>
-                        <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
-                        <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                            microdosing tousled waistcoat.</p>
+                        <h2 class="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">Siguientes Pasos
+                        </h2>
+                        <h1 class="title-font text-lg font-medium text-gray-900 mb-3">El Plan</h1>
+                        <p class="leading-relaxed">Este plan consiste esconderse en el mundo de los humanos como
+                            mortales, aprender de ellos, volverse fuertes con la esperanza de algún día encontrar una de
+                            las puertas y entrar al Jardín y reclamar lo que es de ellos. </p>
                     </div>
                 </div>
             </div>
