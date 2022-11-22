6f56c83a4915d467f78652900713b3c077136411
Rena Salazar <renasalazart@outlook.com>
2022-11-22 11:56:35 -0700

    Textos Social Team

--- resources/views/layouts/partials/social.blade.php
+++ resources/views/layouts/partials/social.blade.php
@@ -2,26 +2,26 @@
     <div class="container px-5 py-24 mx-auto flex flex-wrap">
         <div class="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
             <div class="w-full sm:p-4 px-4 mb-6">
-                <h1 class="title-font font-medium text-xl mb-2 text-gray-900">Moon hashtag pop-up try-hard offal truffaut
+                <h1 class="title-font font-medium text-xl mb-2 text-gray-900">Nuestros Datos
                 </h1>
-                <div class="leading-relaxed">Pour-over craft beer pug drinking vinegar live-edge gastropub, keytar neutra
-                    sustainable fingerstache kickstarter.</div>
+                <div class="leading-relaxed">Datos recogidos con distintos tipos de intrumentos sobre el engage del
+                    juego.</div>
             </div>
             <div class="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
-                <h2 class="title-font font-medium text-3xl text-gray-900">2.7K</h2>
-                <p class="leading-relaxed">Users</p>
+                <h2 class="title-font font-medium text-3xl text-gray-900">54</h2>
+                <p class="leading-relaxed">Usuarios</p>
             </div>
             <div class="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
-                <h2 class="title-font font-medium text-3xl text-gray-900">1.8K</h2>
-                <p class="leading-relaxed">Subscribes</p>
+                <h2 class="title-font font-medium text-3xl text-gray-900">100</h2>
+                <p class="leading-relaxed">Seguidores</p>
             </div>
             <div class="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
                 <h2 class="title-font font-medium text-3xl text-gray-900">35</h2>
-                <p class="leading-relaxed">Downloads</p>
+                <p class="leading-relaxed">Descargas</p>
             </div>
             <div class="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
                 <h2 class="title-font font-medium text-3xl text-gray-900">4</h2>
-                <p class="leading-relaxed">Products</p>
+                <p class="leading-relaxed">Producto</p>
             </div>
         </div>
         <div class="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
